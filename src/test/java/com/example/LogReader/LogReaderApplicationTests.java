467c47b6090264efad4c024d1b6746868591c8ee
Hari gupta <Haridasgupta09.com>
2025-03-11 05:46:26 +0530

    first commit

---
+++ src/test/java/com/example/LogReader/LogReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LogReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
